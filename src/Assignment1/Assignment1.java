8f6bc8fd87172c1e99350111a840c343fc793fc3
manukj <manu1998kj@gmail.com>
2019-10-27 23:31:37 +0530

    Assignment 1 Completed with all the test cases

---
+++ src/Assignment1/Assignment1.java
@@ -0,0 +1,23 @@
+package Assignment1;
+
+import java.util.List;
+
+public class Assignment1 {
+
+	public static int main(String[] args) throws Exception {
+		ParseArguments parameter;
+
+		// parse the command line arguments in the constructor
+		parameter = new ParseArguments(args);
+		parameter.getMoreInput();
+		List<Items> items = parameter.getItems();
+
+		for (Items item : items) {
+			item.calculate_tax();
+			item.display_iteam_info();
+		}
+		return 1;
+
+	}
+
+}
